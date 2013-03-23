8790f280259e867a96b8085497cd2faff7a8430c
Stian Drøbak <sum41rules4@hotmail.com>
2013-03-24 00:19:57 +0100

    Can now change the color of the flashlight activity

--- src/com/stiandrobak/FlashLight/HelloActivity.java
+++ src/com/stiandrobak/FlashLight/HelloActivity.java
@@ -1,6 +1,7 @@
 package com.stiandrobak.FlashLight;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -17,20 +18,27 @@ public class HelloActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        /*
         LinearLayout x = (LinearLayout)findViewById(R.id.mainStyle);
         x.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                float x = event.getX();
-                float y = event.getY();
+                int x = (int)event.getX();
+                int y = (int) event.getY();
                 float width = v.getWidth();
                 float height = v.getHeight();
-                v.setBackgroundColor(0);
-                return false;  //To change body of implemented methods use File | Settings | File Templates.
+                if(x < (width * (1.0/3) )){
+                    v.setBackgroundColor(Color.rgb(x,y,255));
+                }else if(x < (width * (2.0 / 3))){
+                    v.setBackgroundColor(Color.rgb(255,x,y));
+                }else{
+                    v.setBackgroundColor(Color.rgb(y,255,x));
+                }
+
+                return true;
+
+                //return false;  //To change body of implemented methods use File | Settings | File Templates.
             }
         });
-        */
         Window mWindow = getWindow();
         WindowManager.LayoutParams lp = mWindow.getAttributes();
         lp.screenBrightness = 1;
