f286023b457204943e298012e2c476263263dd85
Stian Drøbak <sum41rules4@hotmail.com>
2013-03-24 00:42:26 +0100

    Added menu to quickly reset to white, and change between Red, Blue, and Green.

--- src/com/stiandrobak/FlashLight/HelloActivity.java
+++ src/com/stiandrobak/FlashLight/HelloActivity.java
@@ -3,12 +3,8 @@ package com.stiandrobak.FlashLight;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
+import android.view.*;
 import android.widget.LinearLayout;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class HelloActivity extends Activity {
     /**
@@ -25,7 +21,7 @@ public class HelloActivity extends Activity {
                 int x = (int)event.getX();
                 int y = (int) event.getY();
                 float width = v.getWidth();
-                float height = v.getHeight();
+                //float height = v.getHeight();
                 if(x < (width * (1.0/3) )){
                     v.setBackgroundColor(Color.rgb(x,y,255));
                 }else if(x < (width * (2.0 / 3))){
@@ -38,11 +34,39 @@ public class HelloActivity extends Activity {
 
                 //return false;  //To change body of implemented methods use File | Settings | File Templates.
             }
+
         });
         Window mWindow = getWindow();
         WindowManager.LayoutParams lp = mWindow.getAttributes();
         lp.screenBrightness = 1;
         mWindow.setAttributes(lp);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater =  getMenuInflater();
+        inflater.inflate(R.menu.my_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        LinearLayout layout = (LinearLayout)findViewById(R.id.mainStyle);
+        switch(item.getItemId()){
+            case R.id.reset:
+                layout.setBackgroundColor(Color.WHITE);
+                return true;
+            case R.id.setBlue:
+                layout.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.setRed:
+                layout.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.setGreen:
+                layout.setBackgroundColor(Color.GREEN);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
